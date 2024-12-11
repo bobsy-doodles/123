08eb20a353059f767ea6c02ed64144682b7b1bf9
kuna <69559852+Kuna-Labs@users.noreply.github.com>
2024-12-11 18:19:42 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,29 @@
+setScreen("screen1");
+onEvent("screen1", "click", function( ) {
+  stopTimedLoop();
+  setScreen("screen2");
+});
+if ("") {
+  
+} else {
+  onEvent("label1", "click", function( ) {
+    open("https://scratch.mit.edu/users/bobsy13_/");
+    setScreen("screen1");
+  });
+  onEvent("label2", "click", function( ) {
+    open("https://scratch.mit.edu/projects/1037733090/");
+    setScreen("screen1");
+  });
+  onEvent("label3", "click", function( ) {
+    open("https://scratch.mit.edu/projects/1023754851/");
+    setScreen("screen1");
+  });
+  onEvent("label4", "click", function( ) {
+    open("https://scratch.mit.edu/projects/1024548376/");
+    setScreen("screen1");
+  });
+  onEvent("label5", "click", function( ) {
+    open("https://scratch.mit.edu/projects/1038708063/");
+    setScreen("screen1");
+  });
+}
